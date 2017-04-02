404b40019df24d41a756ca6bf0cd6742ecc830ca
Connor Wyatt <connorwyatt1@gmail.com>
2017-04-02 23:50:33 +0100

    feat(home): add part of the home page content

--- src/components/CwAppRoot.jsx
+++ src/components/CwAppRoot.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import CwHeader from './CwHeader.jsx';
+import CwHome from './CwHome.jsx';
 import CwFooter from './CwFooter.jsx';
 
 export default class CwAppRoot extends React.Component {
   render() {
     return <div>
       <CwHeader/>
+      <CwHome/>
       <CwFooter/>
     </div>;
   }
---
+++ src/components/CwHome.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const CwHome = () => (
+  <div>
+    <p>Senior \ Web \ Application \ Developer</p>
+    <p>Nottingham, UK</p>
+  </div>
+);
+
+export default CwHome;
