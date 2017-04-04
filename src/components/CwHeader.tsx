85c75e340fc9faf52e0edda91d77cac2d937a9c1
Connor Wyatt <connorwyatt1@gmail.com>
2017-04-04 22:03:00 +0100

    feat(CwHeader): add basic styling and structure

--- src/components/CwHeader.tsx
+++ src/components/CwHeader.tsx
@@ -1,7 +1,21 @@
 import React, {StatelessComponent} from 'react';
+import './CwHeader.scss';
 
 export const CwHeader: StatelessComponent<null> = () => {
-  return <header>
-    <h1>Connor Wyatt IO</h1>
+  const links = [].map(link => {
+    return <a className="cw-header--link" key={link}>{link}</a>;
+  });
+
+  return <header className="cw-header">
+    <div className="cw-header--container">
+      <span className="cw-header--branding">
+        <h1 className="cw-header--title">
+          <span className="cw-header--title__bold">Connor</span> Wyatt IO
+        </h1>
+      </span>
+      <nav className="cw-header--links">
+        {links}
+      </nav>
+    </div>
   </header>;
 };
