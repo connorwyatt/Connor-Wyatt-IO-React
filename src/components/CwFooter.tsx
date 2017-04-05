e3132f7c453ae5237da67e0b4cc46f8f644fdb78
Connor Wyatt <connorwyatt1@gmail.com>
2017-04-05 22:34:30 +0100

    feat(footer): add content to footer

--- src/components/CwFooter.tsx
+++ src/components/CwFooter.tsx
@@ -15,6 +15,30 @@ export class CwFooter extends Component<IProps, IState> {
 
   render() {
     return <footer className="cw-footer">
+      <div className="cw-footer--content">
+        <div className="cw-footer--section">
+          <div className="cw-footer--title">Contact Me</div>
+
+          <div className="cw-footer--linkContainer">
+            <a href="mailto:connorwyatt1@gmail.com"
+               className="cw-footer--link">
+              <span className="cw-footer--linkText">connorwyatt1@gmail.com</span>
+            </a>
+          </div>
+
+          <div className="cw-footer--linkContainer">
+            <a href="https://github.com/connorwyatt"
+               className="cw-footer--link">
+              <span className="cw-footer--linkText">GitHub</span>
+            </a>
+          </div>
+        </div>
+        <div className="cw-footer--section">
+          <div className="cw-footer--title">Bio</div>
+
+          <div className="cw-footer--text">I am a huge Formula 1 fan and I enjoy programming in my spare time. I also enjoy photography.</div>
+        </div>
+      </div>
       <div className="cw-footer--copyright">
         Copyright &copy; Connor Wyatt {this.state.year} - All rights reserved.
       </div>
