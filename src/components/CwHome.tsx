e21d708e9e7e265756fc48c69eb5b57a9f631c44
Connor Wyatt <connorwyatt1@gmail.com>
2017-04-05 22:49:21 +0100

    feat(home): add panel with "Who Am I?"

--- src/components/CwHome.tsx
+++ src/components/CwHome.tsx
@@ -10,5 +10,17 @@ export const CwHome: StatelessComponent<null> = () => (
         <div className="cw-home--hero-image-subtitle">Nottingham, UK</div>
       </CwHeroImage>
     </div>
+
+    <div className="cw-home--grid">
+      <div className="cw-home--grid-item">
+        <div className="cw-home--panel">
+          <h2>Who Am I?</h2>
+          <p>I am a Senior Web Application Developer that works with front-end and back-end systems as well as graphic
+            design.</p>
+          <p>Because of this I am capable of designing, coding and maintaining modern, tailored, responsive and
+            breath-taking websites from start to finish.</p>
+        </div>
+      </div>
+    </div>
   </div>
 );
