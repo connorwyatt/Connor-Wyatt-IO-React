65b96a738817f634555da781695cf282ee0e4b78
Connor Wyatt <connorwyatt1@gmail.com>
2017-04-09 09:42:06 +0100

    feat(forms): add form buttons to form

--- src/components/CwForm.tsx
+++ src/components/CwForm.tsx
@@ -1,10 +1,12 @@
-import React, {StatelessComponent} from 'react';
+import React, {StatelessComponent, FormEvent} from 'react';
 import {IFieldComponentConfig, IFormControl} from '../interfaces';
+import './CwForm.scss';
 
 interface IProps {
   name: string;
   form: IFormControl;
   fieldComponentConfig: Array<IFieldComponentConfig<any>>;
+  onSubmit?: (event: FormEvent<HTMLFormElement>) => void;
 }
 
 export const CwForm: StatelessComponent<IProps> = props => {
@@ -15,7 +17,22 @@ export const CwForm: StatelessComponent<IProps> = props => {
     return React.createElement(componentType, {key: fieldName, id: fieldName, fieldControl: control});
   });
 
-  return <form name={props.name} noValidate={true}>
+  return <form name={props.name} onSubmit={e => handleSubmit(e, props)} noValidate={true}>
     {formControls}
+
+    <div className="cw-form--form-buttons">
+      <button className="cw-form--form-button"
+              disabled={!props.form.isDirty && !props.form.isValid}
+              type="submit">Submit
+      </button>
+    </div>
   </form>;
+};
+
+const handleSubmit = (event: FormEvent<HTMLFormElement>, props: IProps) => {
+  event.preventDefault();
+
+  if (props.onSubmit) {
+    props.onSubmit(event);
+  }
 };
